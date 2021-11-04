fd43c54ae71fbd4fcfedbb3758b0bbf280af1f18
Bescore <jeanmar-97.1@hotmail.fr>
2021-11-04 17:50:02 +0100

    construction des pages de connexion, s'incrire , il reste beaucoupd e travail

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/labombe">Bomba</router-link> |
+    <router-link to="/">Se connecter</router-link> |
+    <router-link to="/inscrire">s'inscrire</router-link> |
   </div>
   <router-view/>
 </template>
@@ -17,15 +16,19 @@
 }
 
 #nav {
-  padding: 30px;
+  padding:50px;
 
   a {
     font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #db590d;
     }
   }
+}
+.groupopict{
+  width:350px;
+  height:350px;
 }
 </style>
---
+++ src/views/connecter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img class="groupopict" src="@\assets\icon-above-font.png">
+    <connecter msg="Connectons-nous !"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import connecter from '@/components/connecter.vue'
+
+export default {
+  name: 'connection',
+  components: {
+    connecter
+  }
+}
+</script>
