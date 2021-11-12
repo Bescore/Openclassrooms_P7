88c7297e877896b713cca79294ed012f968f3365
Bescore <jeanmar-97.1@hotmail.fr>
2021-11-12 11:07:59 +0100

    progression, reoganisation, à noter , j'ai trouvé coment manipuler les queries pour afficher les données de ma base via mon back

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,32 @@
 const express = require( 'express' );
+const con=require('./mysql/db')
 
 const app = express();
+
+app.use( ( req, res, next ) => {
+    res.setHeader( 'Access-Control-Allow-Origin', '*' );
+    res.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization' );
+    res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS' );
+    next();
+} );
+
+
+app.use( '/api/stuff', ( req, res, next ) => {
+      con.query(
+        'SELECT * FROM utilisateurs WHERE email = ?',
+        [ 'jm@live.fr' ],
+        function ( err, results ) {
+
+            res.status(200).json( results[ 0 ].prenom  )
+
+        } )
+    
+} );
+
+
+
+
+
 
 
 
---
+++ Backend/mysql/db.js
@@ -0,0 +1,48 @@
+const mysql = require( 'mysql2' );
+
+
+const con = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: 'carnibarge97',
+    database:'projet7'
+} );
+
+con.connect( ( err ) => {
+    if ( err ) {
+        console.log( 'Erreur de connection à la base de donnée !' );
+        return;
+    }
+    console.log( 'Base de donnée connectée !' );
+} );
+
+/*EXEMPLE D'UTILISATION DES QUERY ET MANIPULATION
+const kiki = con.query(
+    'SELECT * FROM utilisateurs WHERE email = ?',
+    [ 'jm@live.fr' ],
+    function ( err, results ) {
+        
+        console.log( JSON.stringify( results[ 0 ].md_passe ) )
+      
+    }
+);*/
+
+
+/*EXEMPLE DE FONCTION QUI AFFICHE LA BASE DEPUIS NODE.JS
+
+app.use( '/api/stuff', ( req, res, next ) => {
+      con.query(
+        'SELECT * FROM utilisateurs WHERE email = ?',
+        [ 'jm@live.fr' ],
+        function ( err, results ) {
+
+            res.status(200).json( results[ 0 ].prenom  )
+
+        } )
+
+} );*/
+
+
+
+
+module.exports = con;
