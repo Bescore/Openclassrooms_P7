2ee948a4aef98cc28e4c576bc6461f1570648579
Bescore <jeanmar-97.1@hotmail.fr>
2021-11-25 17:17:22 +0100

    ajout d'express rate limit

--- Backend/app.js
+++ Backend/app.js
@@ -5,10 +5,12 @@ const userRoutes=require('./routes/user_route')
 const path = require( 'path' );
 const bodyParser=require('body-parser')
 const helmet = require( "helmet" );
+const rateLimit = require( "express-rate-limit" );//anti ddos
 
-
+//sécurité helmet//
 app.use( helmet() );
 
+//sécurité cors abaissée//
 app.use( ( req, res, next ) => {
     res.setHeader( 'Access-Control-Allow-Origin', '*' );
     res.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization' );
@@ -16,12 +18,22 @@ app.use( ( req, res, next ) => {
     next();
 } );
 
+//anti ddos/limiter//
+const limiter = rateLimit( {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+} );
+//  apply to all requests
+app.use( limiter );
+
+//body parser pour traiter les objets//
 app.use( bodyParser.json() );
 
 
-
+//ligne lié à multer, va servir le dossier indiqué//
 app.use( "/image", express.static( path.join( __dirname, 'image' ) ) );
 
 app.use( '/', elementRoutes );
 app.use( '/auth',userRoutes );
+
 module.exports = app;
